37264ffff5a11b339c7380f4d0046553bd872d48
jcastan6 <josecastanon4@gmail.com>
2021-09-26 22:17:25 -0700

    fixed header image

--- src/client/Components/Header/Header.js
+++ src/client/Components/Header/Header.js
@@ -32,7 +32,7 @@ class Header extends Component {
       <Navbar className="navbar" expand="lg">
         <Navbar.Brand>
           <img
-            src="https://00f74ba44b70baeb6018bccc0ce90629f7e1f5f865-apidata.googleusercontent.com/download/storage/v1/b/ezvend/o/cvm.png?jk=AFshE3WXVN609aUgr9nGTV2W-UY4Whr7F7eFnTH2sYBQW_JxC2VWQJhq2KYk0dXYd10L326WhvnwhC5sKej-FrIQ7-wdCuQzCv4mOLLa3sduJazK5rsFwLjQNKqVTiRCJqj6cC8NDPOtRc0sglBe_5wJHVSRB59wwbFht4zUHuJ3cViPFomf_5PRnMvtRMBmXKVAm6aeOKgjBnPBAp_5MxOrIya9ZI9aCMU--h5POuT9Z2nJQY-6vYSixD6y-4HmNrafeCGP37mqyH3Q_iNqxxLZ043WxLUhUrm0PQAQxgWQbxsx3ZMvSadPPy7jSQgW05HUZPWQMmhC7E7-xemCJa_D1BwsW_JsYZfsiINQhlGNQAJhQZLkccqBWqHD8KzejHh-ofmOtwqD6MUXoORU6RfVX3N_77kepudQN1gmI_gv8GRnWgePdru0LcUbcwJW9Xjh40Yb91uqYzq-7DtmUnGhb6VADOmLoOc9JONIkhTV9Jazy_T_WpQRT-3C0tbzt3S2QeR2tte04ygYFEcAC40HSZ9NB_iL1GPVDgvx76JGyQC_WJSFHkm_VZUb6IaD39OjQi1yH1G2EsReHmoolXlaXDWMHwtF35tYs2a6N-s5kxA9TlrhxhBoPzUvUTTHQoziQ9c9876vcHGFurKKOww-xCTugesmITNeiY1TbdE82t00UdawFlLNhZzN_vQC9vfh8GrWe8mgaVgTi-9Teyo9nNmk4O8Nvwt4T4bQ2icyN9SyKCiYH0IvZp46d9QoefBSk_0nLIVUnW8UESQK24xSSmhKwdUVPuVsG6UxTTbQ7S3t9iN2fSjsDHSnyXcG_kXwU21Lzv-7d2uI2sOpwx1Tb1KPxOt1-FHvIggkykJ72Aw5tr6-hlKYZKzPUvCJD2QzIYy6QCe-zQzgNWC__9aw3JH6C-ufi1qISAEkvfAa7Fpsz09Q&isca=1"
+            src="https://storage.googleapis.com/ezvend/cvm.png"
             width="100"
             className="d-inline-block align-top"
             alt="CVM"
